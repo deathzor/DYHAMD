39cc18512350d6978454d5bf68e7535cc2b41d3d
deathzor <death@deathzor.com>
2012-12-13 20:00:53 +0000

    Added a all the nice warrenty / warning stuff.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import filesystem
 import blockdevice
+print "WARING: this system provides no encryption what so ever in its current stage so it provides Zero in terms of security and posible overwrites it one file data there for this should not be used on any production system";
+print "WARING THIS CODE IS PRE-ALPHA AND SHOULD BE CONSIDERED TO BE WITHOUT ANY WARRENTY SO I'm NOT RESPONSIBLE FOR ANY DAMAGE DIRECT OR INDIRECT DONE TO ANYTHING BY THE USAGE OF THIS CODE"; 
+
 blockdev = blockdevice.blockdevice('/tmp/ruberfuse');
 ruberfuse = filesystem.main(blockdev);
 #this line is to create a new index table

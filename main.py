15dc69a8a7b3c3ba59f9365da9c1691add0b5695
deathzor <death@deathzor.com>
2012-12-13 18:34:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import filesystem
-ruberfuse = filesystem.main();
-ruberfuse.setblockdevice('/tmp/ruberfuse');
+ruberfuse = filesystem.main('/tmp/ruberfuse');
 #this line is to create a new index table
 #ruberfuse.create_table_index('mykey2');
 ruberfuse.load_table_index('mykey2');
 if (ruberfuse.create_file_index('myname2','this is my file') != 0):
 	print 'error creating file';
-	print ruberfuse._findfile();
+print ruberfuse._findfile();
 
